a61a351066c695b3b07196fd655d00631f06d592
siwoo.kim@davinci-retail.com <siwoo.kim@davinci-retail.com>
2019-08-18 15:02:58 -0400

    Brute-force(Exhaustive search with Recursion)

--- src/main/java/com/siwoo/algorithm/paradigm/BruteForce.java
+++ src/main/java/com/siwoo/algorithm/paradigm/BruteForce.java
@@ -10,6 +10,13 @@ import java.util.function.Predicate;
  * Brute-Force (Exhaustive search)
  *  :컴퓨터의 빠른 계산 능력을 이용해 가능한 경우의 수를 일일히 나열하면서 답을 찾는 방법.
  *
+ *  완전 탐색 레시피.
+ *      :어떤 문제를 완전 탐색으로 해결하기 위한 필요 과정.
+ *          1. 완전 탐색은 존재하는 모든 답을 하나씩 검사한다.
+ *          2. 가능한 모든 답의 후보를 만드는 과정을 '여러 개의 선택'으로 나눈다.
+ *          3. 그중 하나의 조각을 선택해 답의 일부를 만들고, 나머지 답은 '재귀 호출' 을 통해 완성한다.
+ *          4. 조각이 하나밖에 남지 않은 경우(혹은 쪼개질 수 없는 경우), 이것을 기저 사례로 선택해 처리.
+ *
  *  재귀 호출 (recursive function) 과 완전 탐색.
  *      :수행하는 작업의 범위가 작아질수록 형태가 유사해지는 작업의 특성을 이용해
  *      수행할 작업을 유사한 형태의 여러 조각으로 쪼갠 뒤 그 중 한 조각을 수행하고
@@ -17,6 +24,7 @@ import java.util.function.Predicate;
  *
  *      1. 문제를 분할하라.
  *      2. 기저 사례를 선택하라.
+ *
  *
  *
  */
@@ -190,6 +198,7 @@ public class BruteForce {
 
         /**
          * 해당 위치에서 주어진 단어의 첫 문자가 주어진 좌표에서 시작하는지를 반환.
+         * 시간 복잡도는 단어의 길이 N 에 대해 O(8^N)
          * @param p 주어진 좌표
          * @param word 검색 문자
          * @return
@@ -212,4 +221,58 @@ public class BruteForce {
         }
     }
 
+    static class Pairing {
+        private boolean[][] friendships;
+        private int STUDENT_NUM;
+
+        /**
+         * 모든 학생을 '친구인 두 명'씩 짝지을 수 있는 모든 경우의 수를 리턴.
+         * 재기 호출과 중복.
+         *  짝을 지을 시 (0,1) 후에 (2,3)을 짝짓는 것은 (2,3) 이후에 (0.1) 을 짝짓는 것과 같지만
+         *  아래 코드는 중복을 허용한다.
+         *  재귀 호출에서는 중복으로 세는 이런 경우가 매우 흔하다.
+         *  이 속성을 강제하기 위해선 각 단계에서 남아 있는 학생들 중 '가장 번호가 빠른' 학생의 짝을 찾아주도록 한다.
+         *      (특정 순서를 이용하여 재귀에서 중복을 허용치 않게 하라.)
+         *  이렇게 되면 (1,0) 은 허용치 않으며, 항상 빠른 학생부터 번호가 찍기에 (2,3), (0,1) 의 순서로는 짝이 지어지지 않는다.
+         *
+         * @param taken
+         * @return
+         */
+//        int countPairings(boolean[] taken) {
+//            boolean f = false;
+//            for (int i=0; i<STUDENT_NUM; i++)
+//                f = taken[i] | f;
+//            if (f) return 1;
+//            int r = 0;
+//            for (int i=0; i<STUDENT_NUM; i++)
+//                for (int j=0; j<STUDENT_NUM; j++) {
+//                    if (!taken[i] && !taken[j] && friendships[i][j]) {
+//                        taken[i] = taken[j] = true;
+//                        r += countPairings(taken);
+//                        taken[i] = taken[j] = false;
+//                    }
+//                }
+//            return r;
+//        }
+        int countPairings(boolean[] taken) {
+            // 남은 학생들 중 가장 번호가 빠른 학생을 찾는다(중복 방지)
+            int firstFree = -1;
+            for (int i=0; i<taken.length; i++)
+                if (!taken[i]) {
+                    firstFree = i;
+                    break;
+                }
+            //base case: 모든 학생이 짝을 찾았으면 한 가지 방법을 찾았으니 종료한다.
+            if (firstFree == -1) return 1;
+            int r = 0;
+            for (int pair=firstFree+1; pair<STUDENT_NUM; pair++) {
+                if (!taken[pair] && friendships[firstFree][pair]) {
+                    taken[pair] = taken[firstFree] = true;
+                    r += countPairings(taken);
+                    taken[pair] = taken[firstFree] = false;
+                }
+            }
+            return r;
+        }
+    }
 }
